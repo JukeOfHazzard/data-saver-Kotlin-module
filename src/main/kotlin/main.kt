b93970109bfd20ac48cb0f5c3b269ae8651b7be2
JukeOfHazzard <elijukes1451@gmail.com>
2021-01-28 16:53:29 -0700

    getting the start of the module done

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,20 @@
+/**/
+import java.util.Random /*this will give the */
+
+class spell(r: Int, type: String, id: String){
+    private val range : Int = r
+    private val dmgType : String = type
+    private val name : String = id
+}
+
+class encounter(){
+    private val distance : Int = 50
+    private val alive : Boolean = true
+}
+
 fun main(args: Array<String>) {
     println("Hello World!")
+    val fire = spell(120, "fire","Fire Bolt") /*note that strings can't use the single quotes to show*/
+    val ice = spell(60, "cold","Frost Ray")
+    val enemy = encounter()
 }
